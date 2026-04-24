a786ba57d6a0048aaf76a56bcabf4016acb85b62
rudcoa <rudy.coarna@edu.uah.es>
2026-04-24 09:42:28 +0200

    Se implementan más métodos que siguen en pendiente de revisar dado a la estructura del árbol...

--- src/clases/ArbolBinarioDeBusqueda.java
+++ src/clases/ArbolBinarioDeBusqueda.java
@@ -57,39 +57,77 @@ public class  ArbolBinarioDeBusqueda<T extends Comparable<T>> implements Interfa
         }
     }
 
-    /* Metodo para obtener el subárbol de la izquierda
+    // Metodo para obtener el subárbol de la izquierda - SON METODOS EQUIVALENTES
     public ArbolBinarioDeBusqueda<T> getSubArbolIzquierda() {
-
+        Nodo<T> copia = raiz;
+        // 1. HAY QUE VER QUE LA RAIZ NO ESTÉ VACÍA
+        if (copia == null) {
+            // No se puede devolver ningún subárbol binario...
+            System.out.println("No hay subárboles que se puedan devolver ya que este arbol no contiene ningún dato");
+        }
+        // 2. EN CASO DE QUE NO ESTÉ VACÍA -> Izquierda
+        Nodo<T> nuevaraiz = copia.getIzquierda();
+        if (nuevaraiz == null) {
+            System.out.println("Este subárbol está vacío");
+        }
+        ArbolBinarioDeBusqueda<T> arbol = new ArbolBinarioDeBusqueda<>(nuevaraiz);
+        return arbol;
     }
-    */
 
-
-    /* Metodo para obtener el subárbol de la derecha
+    // Metodo para obtener el subárbol de la derecha - SON METODOS EQUIVALENTES
     public ArbolBinarioDeBusqueda<T> getSubArbolDerecha() {
-
+        Nodo<T> copia = raiz;
+        //1. TENEMOS QUE VER QUE LA RAÍZ NO ESTÉ VACÍA
+        if (copia == null) {
+            // No se puede devolver ningún subárbol binario...
+            System.out.println("No hay subárboles que se puedan devolver");
+        }
+        // 2. EN CASO DE QUE NO ESTÉ VACÍA -> Derecha
+        Nodo<T> nuevaraiz = copia.getDerecha();
+        if (nuevaraiz == null) {
+            System.out.println("Este subárbol está vacío");
+        }
+        ArbolBinarioDeBusqueda<T> arbol = new ArbolBinarioDeBusqueda<>(nuevaraiz);
+        return arbol;
     }
-     */
 
     // Metodo para comprobar si el árbol está vacio
     public boolean isArbolEmpty() {
-        // Siempre falso
-        boolean b = false;
         // Si el único nodo del árbol está vacío (debería de ser lo mismo que preguntar, "¿hay raíz?"), entonces, es verdad
-        if (raiz == null) {
-            b = true;
-        }
-        return b;
+        return raiz == null;
     }
 
     // METODOS QUE HAN SIDO IMPLEMENTADOS POR LA INTERFAZ
     @Override
     public int getGrado() {
-        return 0;
+        // HAY QUE CONSIDERAR LOS TRES TIPOS DE CASOS
+        return 2;       // lo dejaremos como 2 de momento, luego habria que considerar las posibilidades de que sea: 0 (nodo hoja), 1 ó 2 (nodo rama/nodo raiz)
     }
 
     @Override
     public int getAltura() {
-        return 0;
+        Nodo<T> copia = raiz;
+        // Inicializamos unos contadores que simbolizan qué hay a la derecha y a la izquierda
+        int izquierda = 0;
+        int derecha = 0;
+
+        // Ahora lo que tenemos que hacer es preguntar recursivamente en cada uno, "¿quién tiene más, la derecha, o la izquierda?"
+        while (copia != null || izquierda < 2) {
+            // Consigo el subarbol que contiene toda la rama izquierda
+            copia = copia.getIzquierda();
+            izquierda = izquierda + 1;
+        }
+
+        while (copia != null || derecha < 2) {
+            // Consigo el subárbol que contiene toda la rama derecha
+            copia = copia.getDerecha();
+            derecha = derecha + 1;
+        }
+
+        if (izquierda > derecha) {
+            return izquierda;
+        }
+        return derecha;
     }
 
     @Override
@@ -117,23 +155,19 @@ public class  ArbolBinarioDeBusqueda<T extends Comparable<T>> implements Interfa
         return List.of();
     }
 
-    @Override
-    public List<T> getListaPreorden() {
-        return List.of();
-    }
-
-    @Override
-    public List<T> getListaPostorden() {
-        return List.of();
-    }
-
-    @Override
-    public List<T> getListaOrdenCentral() {
-        return List.of();
-    }
-
     // SEA LA LISTA: [9,7,3,5,8,10,22]
-    // PREORDEN: (sale la misma lista)
-    // POSTORDEN: [5,3,8,7,22,10,9]
-    // ORDEN CENTRAL: [3,5,7,8,9,10,22] (lista ordenada)
+    @Override
+    public List<T> getListaPreorden() {         // PREORDEN: (sale la misma lista)
+        return List.of();
+    }
+
+    @Override
+    public List<T> getListaPostorden() {        // POSTORDEN: [5,3,8,7,22,10,9]
+        return List.of();
+    }
+
+    @Override
+    public List<T> getListaOrdenCentral() {     // ORDEN CENTRAL: [3,5,7,8,9,10,22] (lista ordenada)
+        return List.of();
+    }
 }
